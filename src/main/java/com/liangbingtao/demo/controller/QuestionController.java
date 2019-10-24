59d5523608037ea33c9e2fa5e8e313572d4cd5d8
liangbingtao <1607247596@qq.com>
2019-10-24 18:09:05 +0800

    回复列表

--- src/main/java/com/liangbingtao/demo/controller/QuestionController.java
+++ src/main/java/com/liangbingtao/demo/controller/QuestionController.java
@@ -1,8 +1,10 @@
 package com.liangbingtao.demo.controller;
 
+import com.liangbingtao.demo.dto.CommentDTO;
 import com.liangbingtao.demo.dto.QuestionDTO;
 import com.liangbingtao.demo.mapper.QuestionMapper;
 import com.liangbingtao.demo.model.Question;
+import com.liangbingtao.demo.service.CommentService;
 import com.liangbingtao.demo.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,16 +12,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 @Controller
 public class QuestionController {
 
     @Autowired
     private QuestionService questionService;
 
+    @Autowired
+    private CommentService commentService;
+
     @GetMapping("/question/{id}")
     public String question(@PathVariable(name = "id") Long id,
                            Model model){
         QuestionDTO questionDTO=questionService.getById(id);
+
+        List<CommentDTO> comments = commentService.listByQuestionId(id);
+
         //累加阅读数
         questionService.incView(id);
         model.addAttribute("question",questionDTO);
